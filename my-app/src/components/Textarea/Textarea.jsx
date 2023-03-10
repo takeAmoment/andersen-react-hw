8577b426b99113ff85d23798643b95e4c7acab7e
Aliaksandra <alekspekhota@gmail.com>
2023-03-10 16:15:31 +0200

    feat: create Textarea component

---
+++ my-app/src/components/Textarea/Textarea.jsx
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+
+export default class Textarea extends Component {
+  render() {
+    const { item, handleChange, value } = this.props;
+    return (
+      <textarea
+        rows={5}
+        placeholder={item.placeholder}
+        name={item.name}
+        id={item.id}
+        onChange={handleChange}
+        value={value}
+        required
+      />
+    );
+  }
+}
