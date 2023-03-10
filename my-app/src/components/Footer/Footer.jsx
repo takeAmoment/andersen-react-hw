392fa000f7a6c2e195b53aaf04762ded5c414e89
Aliaksandra <alekspekhota@gmail.com>
2023-03-10 13:29:30 +0200

    feat: create Footer component

---
+++ my-app/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import styles from './Footer.module.css';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div className={styles.footer__container}>
+          <p className={styles.footer__text}>©2023 Created by Aliaksandra Piakhota</p>
+        </div>
+      </footer>
+    );
+  }
+}
