9d3b30d007f08c217a306b8ef8203b93d512d8dc
Aliaksandra <alekspekhota@gmail.com>
2023-03-11 14:06:11 +0200

    feat: create Modal

--- my-app/src/components/Button/Button.jsx
+++ my-app/src/components/Button/Button.jsx
@@ -2,12 +2,13 @@ import { Component } from 'react';
 
 export default class Button extends Component {
   render() {
-    const { buttonType, classname, disabled, text } = this.props;
+    const { buttonType, classname, disabled, text, onClick } = this.props;
     return (
       <button
         type={buttonType === 'submit' ? 'submit' : 'button'}
         className={classname}
         disabled={disabled}
+        onClick={onClick}
       >
         {text}
       </button>
---
+++ my-app/src/components/Modal/Modal.jsx
@@ -0,0 +1,27 @@
+import ReactDOM from 'react-dom';
+import { Component } from 'react';
+import styles from './Modal.module.css';
+import Button from '../Button/Button';
+
+const moduleRoot = document.getElementById('root');
+
+export default class Modal extends Component {
+  render() {
+    const { onClose, children } = this.props;
+    return ReactDOM.createPortal(
+      <div
+        className={styles.modal}
+        role="button"
+        tabIndex={0}
+        onClick={onClose}
+        onKeyDown={onClose}
+      >
+        <div className={styles.modal__container}>
+          {children}
+          <Button classname={styles.modal__button} type="button" onClick={onClose} text="Close" />
+        </div>
+      </div>,
+      moduleRoot
+    );
+  }
+}
