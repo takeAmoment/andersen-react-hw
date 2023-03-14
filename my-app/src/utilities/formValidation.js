0a1f34f8762b707494a6bcf077abd9d9d186fb48
Aliaksandra <alekspekhota@gmail.com>
2023-03-14 19:44:35 +0100

    feat: change input erorr message

--- my-app/src/utilities/formValidation.js
+++ my-app/src/utilities/formValidation.js
@@ -1,66 +1,69 @@
-export default function validateSurveyForm(type, value) {
+export default function validateSurveyForm(type, fieldValue) {
+  const value = fieldValue.trim();
   let message = '';
   switch (type) {
     case 'name':
-      value.trim();
       if (value.length === 0) {
-        message = 'Please fill this field!';
+        message = 'Поле пустое. Заполните пожалуйста';
       } else if (value.slice(0, 1) !== value.slice(0, 1).toUpperCase()) {
-        message = 'The first letter must be capitalized';
+        message = 'Первая буква должна быть заглавной';
+      } else if (!value.match(/[a-zа-я ]+$/iu)) {
+        message = 'Допустимы только буквы';
       }
       break;
     case 'surname':
-      value.trim();
       if (value.length === 0) {
-        message = 'Please fill this field!';
+        message = 'Поле пустое. Заполните пожалуйста';
       } else if (value.slice(0, 1) !== value.slice(0, 1).toUpperCase()) {
-        message = 'The first letter must be capitalized';
+        message = 'Первая буква должна быть заглавной';
+      } else if (!value.match(/[a-zа-я ]+$/iu)) {
+        message = 'Допустимы только буквы';
       }
       break;
     case 'dateOfBirthday':
       if (value.length === 0) {
-        message = 'Please fill this field!';
+        message = 'Поле пустое. Заполните пожалуйста';
       }
       break;
     case 'phone':
       if (value.length === 0) {
-        message = 'Please fill this field!';
-      } else if (value.length >= 12) {
-        message = 'Max length 12';
-      } else if (!value.match(/[0-9]{3}-[0-9]{3}-[0-9]{3}/)) {
-        message = 'Your number should be';
+        message = 'Поле пустое. Заполните пожалуйста';
+      } else if (value.length > 12) {
+        message = 'Допустимая длинна 12 символов';
+      } else if (!value.match(/[0-9]{1}-[0-9]{4}-[0-9]{2}-[0-9]{2}/)) {
+        message = 'Допустимый формат 7-7777-77-77';
       }
       break;
     case 'site':
       if (value.length === 0) {
-        message = 'Please fill this field!';
+        message = 'Поле пустое. Заполните пожалуйста';
       } else if (
         !value.match(
-          /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/
+          /https:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/
         )
       ) {
-        message = 'Your link should be';
+        message = 'Введите ссылку. Ссылка должна начинаться с https://';
       }
       break;
     case 'about':
       if (value.length === 0) {
-        message = 'Please fill this field!';
-      } else if (value.length >= 600) {
-        message = 'Max length 600';
+        message = 'Поле пустое. Заполните пожалуйста';
+      } else if (value.length > 600) {
+        message = 'Превышен лимит символов в поле';
       }
       break;
     case 'technologies':
       if (value.length === 0) {
-        message = 'Please fill this field!';
-      } else if (value.length >= 600) {
-        message = 'Max length 600';
+        message = 'Поле пустое. Заполните пожалуйста';
+      } else if (value.length > 600) {
+        message = 'Превышен лимит символов в поле';
       }
       break;
     case 'project':
       if (value.length === 0) {
-        message = 'Please fill this field!';
-      } else if (value.length >= 600) {
-        message = 'Max length 600';
+        message = 'Поле пустое. Заполните пожалуйста';
+      } else if (value.length > 600) {
+        message = 'Превышен лимит символов в поле';
       }
       break;
     default:
