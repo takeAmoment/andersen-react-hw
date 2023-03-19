f3ccb93c40b30e1ea753224b4071247f43459dd2
Aliaksandra <alekspekhota@gmail.com>
2023-03-19 21:42:01 +0100

    feat: create createMaskForPhone function

---
+++ my-app/src/utilities/createMaskForPhone.js
@@ -0,0 +1,11 @@
+const createMaskForPhone = (name, value) => {
+  const val = value.replace(/\D/g, '');
+  const nums = val.split('');
+  const format = `${nums[0] ? nums[0] : ''}${nums[1] ? `-${nums[1]}` : ''}${
+    nums[2] ? nums[2] : ''
+  }${nums[3] ? nums[3] : ''}${nums[4] ? nums[4] : ''}${nums[5] ? `-${nums[5]}` : ''}${
+    nums[6] ? nums[6] : ''
+  }${nums[7] ? `-${nums[7]}` : ''}${nums[8] ? nums[8] : ''}`;
+  return format;
+};
+export default createMaskForPhone;
