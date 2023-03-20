06222bd8eb08bf965143768e8613e1c6eedfcac7
Aliaksandra <alekspekhota@gmail.com>
2023-03-20 20:07:19 +0100

    feat: create createKey function

--- my-app/src/components/Survey/Survey.jsx
+++ my-app/src/components/Survey/Survey.jsx
@@ -6,21 +6,27 @@ export const Survey = ({ props, data }) => {
     return object.label;
   };
 
+  const createKey = () => {
+    const key = new Date();
+    return key.getTime();
+  };
+
   return (
     <div className={styles.survey__card}>
       <h2 className={styles.card__title}>
         {props.name} {props.surname}
       </h2>
       <ul className={styles.card__body}>
-        {Object.keys(props).map((item, index) => {
-          return (
-            // eslint-disable-next-line react/no-array-index-key
-            <li key={index} className={styles.body__item}>
-              <p className={styles.item__label}>{findLabel(item)}</p>
-              <p className={styles.item__text}>{props[item]}</p>
-            </li>
-          );
-        })}
+        {Object.keys(props)
+          .slice(2)
+          .map((item) => {
+            return (
+              <li key={createKey()} className={styles.body__item}>
+                <p className={styles.item__label}>{findLabel(item)}</p>
+                <p className={styles.item__text}>{props[item]}</p>
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
