f01a9d81b3987dfa2fd5a5f3a9de995cf73f3e05
Aliaksandra <alekspekhota@gmail.com>
2023-03-20 11:43:35 +0100

    feat: remove div

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,11 @@ import MainPage from './pages/MainPage/MainPage';
 
 const App = () => {
   return (
-    <div>
+    <>
       <Header />
       <MainPage />
       <Footer />
-    </div>
+    </>
   );
 };
 export default App;
